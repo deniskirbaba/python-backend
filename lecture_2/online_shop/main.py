61aa98a4bb5cd9f233f3cca365dd18728eb341de
deniskirbaba <kirbabadenis@gmail.com>
2024-10-07 22:50:48 +0300

    add prometheus_fastapi_instrumentator for exposing the /metrics endpoint

--- lecture_2/online_shop/main.py
+++ lecture_2/online_shop/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-from lecture_2.online_shop.api.online_shop import router
+from prometheus_fastapi_instrumentator import Instrumentator
 
+from lecture_2.online_shop.api.online_shop import router
 
 app = FastAPI()
 app.include_router(router)
+
+# Initialize and expose the /metrics endpoint
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
